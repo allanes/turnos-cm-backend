af8df71e7f9cc1a2108e2e9022d374cc475890d1
Adrian Llanes <ados.adrian@gmail.com>
2023-01-02 15:22:26 -0300

    cambio endpoints a ingles

--- sql_app/api_v1/api.py
+++ sql_app/api_v1/api.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from sql_app.api_v1.endpoints import consultorios, pacientes, medicos
 
 api_router = APIRouter()
-api_router.include_router(consultorios.router, prefix="/consultorios", tags=["consultorios"])
-api_router.include_router(pacientes.router, prefix="/pacientes", tags=["pacientes"])
-api_router.include_router(medicos.router, prefix="/medicos", tags=["medicos"])
+api_router.include_router(consultorios.router, prefix="/offices", tags=["consultorios"])
+api_router.include_router(pacientes.router, prefix="/patients", tags=["pacientes"])
+api_router.include_router(medicos.router, prefix="/doctors", tags=["medicos"])
 
