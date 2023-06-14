41842b64697f78992db447bb8b19985b2c664e91
Adrian Llanes <ados.adrian@gmail.com>
2023-06-14 12:32:38 -0300

    agregado servidor de ngrok

---
+++ ngrok_app/server.py
@@ -0,0 +1,43 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pyngrok import ngrok, conf
+from dotenv import load_dotenv
+
+import yaml
+
+# Load environment variables
+load_dotenv()
+
+# Set the ngrok auth token and region
+conf.get_default().auth_token = os.getenv("NGROK_AUTH_TOKEN")
+# conf.get_default().region = os.getenv("NGROK_REGION")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def startup_event():
+    config_file = open('ngrok_app\\ngrok.yml')
+    config = yaml.safe_load(config_file)
+    
+    # Open a ngrok tunnel
+    # url = ngrok.connect(int(os.getenv("PORT")),).public_url
+    print(f"Creando tunel...")
+    url = ngrok.connect(**config['tunnels']['tunel_cm_esperanza']).public_url
+    print(f"Public URL: {url}")
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app,port=int(os.getenv("NGROK_SERVER_PORT", 8000)))
